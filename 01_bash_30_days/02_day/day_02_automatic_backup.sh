1bd49fcaa1fc7ed8460fd165dffbb0c95fb4c324
Denchikot123 <75613107+Denchikot123@users.noreply.github.com>
2025-11-27 21:28:44 +0300

    day_02_automatic_backup.sh

--- 01_bash_30_days/02_day/day_02_automatic_backup.sh
+++ 01_bash_30_days/02_day/day_02_automatic_backup.sh
@@ -1,1 +1,17 @@
+#!/bin/bash 
 
+BACKUP_DIR="$HOME/backup"
+SOURCE_FILES=("$HOME/.bashrc" "$HOME/.ssh/config")
+
+if [ ! -d "$BACKUP_DIR" ]; then
+    mkdir -p "$BACKUP_DIR"
+fi
+
+for file in "${SOURCE_FILES[@]}"; do
+    if [ -f "$file" ]; then
+        cp -- "$file" "$BACKUP_DIR"
+        echo "OK: Copied: "$file" -> "$BACKUP_DIR""
+    else
+        echo "ERROR: The file was not found or is not a regular file: "$file""
+    fi
+done
