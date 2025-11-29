f6d58ab1be4e52d25ab3c4fb7f96df77405b6375
Denchikot123 <75613107+Denchikot123@users.noreply.github.com>
2025-11-29 23:00:40 +0300

    day_03_disk_space_monitor.sh

--- 01_bash_30_days/03_day/day_03_disk_space_monitor.sh
+++ 01_bash_30_days/03_day/day_03_disk_space_monitor.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+MAX_USAGE=90
+
+usage=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
+if [ $usage -gt $MAX_USAGE ]; then
+    echo "WARNING: The disk is loaded on ${usage}%"
+    echo "INFO: The 5 largest files:"
+    find / -type f -exec du -sh {} + 2>/dev/null | sort -rh | head -5
+fi
+
+for dir in /tmp /var/tmp /dev/shm; do
+    if [ -d "$dir" ]; then
+        count=$(find "$dir" -type f -size +100M 2>/dev/null | wc -l)
+        if [ $count -gt 0 ]; then
+            echo "Found large files in: $dir: $count"
+        fi
+    fi
+done
