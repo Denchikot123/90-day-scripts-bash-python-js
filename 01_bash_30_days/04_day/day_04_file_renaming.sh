318425dd3aebdc008869b8ec6ebfcb4fbae51221
Denchikot123 <75613107+Denchikot123@users.noreply.github.com>
2025-11-30 22:24:03 +0300

    day_04_file_renaming.sh

--- 01_bash_30_days/04_day/day_04_file_renaming.sh
+++ 01_bash_30_days/04_day/day_04_file_renaming.sh
@@ -1,1 +1,29 @@
+1 Task
+
 #!/bin/bash
+
+for file in *; do
+    if [ -f "$file" ]; then
+        mv "$file" "bak_$file"
+        echo "Renamed $file -> bak_$file"
+    fi
+done
+
+2 Task
+
+#!/bin/bash
+
+for file in *; do
+    if [ -f "$file" ]; then
+        new=$(echo "$file" | tr "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" "abcdefghijklmnopqrstuvwxyz")
+        [ "$file" != "$new" ] && mv "$file" "$new"
+    fi
+done
+
+3 Task
+
+#!/bin/bash
+
+for file in *.html; do
+    [ -f "$file" ] && mv "$file" "${file%.html}.txt"
+done
