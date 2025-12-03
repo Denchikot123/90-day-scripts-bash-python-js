6aba592530b51c0af773a77efdf28126e7aadff5
Denchikot123 <75613107+Denchikot123@users.noreply.github.com>
2025-12-04 01:30:04 +0300

    day_06_critical_error_analyzer.sh

--- 01_bash_30_days/06_day/day_06_critical_error_analyzer.sh
+++ 01_bash_30_days/06_day/day_06_critical_error_analyzer.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+DIR="/var/log"
+ERROR=("error" "critical" "fail" "panic" "denied" "corrupt")
+
+search_log() {
+    local pattern="$1"
+    local file_log=("syslog" "auth.log" "kern.log" "messages")
+
+    for log in ${file_log[@]}; do
+        if [ -f "$DIR/$log" ]; then
+            count=$(grep -i -c "$pattern" "$DIR/$log" 2>/dev/null)
+            [ $count -gt 0 ] && echo "OK: $count matches for '$pattern'"
+        fi
+    done
+}
+
+for pattern in ${ERROR[@]}; do
+    search_log "$pattern"
+done
