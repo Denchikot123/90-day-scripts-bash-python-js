23da1abab59f40403bfc7284a3fa48d5ee890c4f
Denchikot123 <75613107+Denchikot123@users.noreply.github.com>
2025-12-25 23:46:54 +0300

    day_09_network_scanner.sh

--- 01_bash_30_days/09_day/day_09_network_scanner.sh
+++ 01_bash_30_days/09_day/day_09_network_scanner.sh
@@ -5,7 +5,11 @@ echo "=========================="
 
 for ip in {1..254}; do
     ping -c 1 -W 1 192.168.1.$ip > /dev/null 2>&1
-    [ $? -eq 0 ] && echo "[+] 192.168.1.$ip - ALIVE"
+    if [ $? -eq 0 ]; then
+        echo "[+] 192.168.1.$ip - ALIVE"
+    else
+        echo "[-] 192.168.1.$ip - ERROR"
+    fi
 done
 
-echo "[*] Scan completed"
+echo "\n[*] Scan completed"
